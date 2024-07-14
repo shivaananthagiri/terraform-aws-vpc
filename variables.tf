51f3bcea5bac53d1107cd9ace412836aa4acad4a
shivaananthagiri <shiva.ananthagiri12507.com>
2024-07-14 10:21:15 +0530

    terraform vpc

---
+++ variables.tf
@@ -0,0 +1,56 @@
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16" # users can override
+}
+
+variable "enable_dns_hostnames" {
+  type = bool
+  default = true
+}
+
+variable "common_tags" {
+  type = map
+  default = {} # it is optional
+}
+
+variable "vpc_tags" {
+  type = map
+  default = {}
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+variable "igw_tags" {
+    type = map
+  default = {}
+}
+variable "public_subnets_cidr" {
+  type = list
+  validation {
+    condition = length(var.public_subnets_cidr) == 2
+    error_message = "Please give 2 public valid subnet CIDR"
+  }
+}
+
+variable "public_subnets_tags" {
+  default = {}
+}
+
+variable "private_subnets_cidr" {
+  type = list
+  validation {
+    condition = length(var.private_subnets_cidr) == 2
+    error_message = "Please give 2 private valid subnet CIDR"
+  }
+}
+
+variable "private_subnets_tags" {
+  default = {}
+}
+
+
